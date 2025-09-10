4739a71bae7ac69821f117779f6b9c9a4a7b858c
Vidhi Bhutia <139327460+Vidhi-bhutia@users.noreply.github.com>
2025-09-10 06:54:10 +0530

    Uncomment error handling and logging in lambda function

--- lambda.py
+++ lambda.py
@@ -4,8 +4,6 @@ import boto3
 print('Loading function')
 s3 = boto3.client('s3')
 def lambda_handler(event, context):
-#print("Received event: " + json.dumps(event, indent=2))
-# Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
@@ -14,6 +12,5 @@ def lambda_handler(event, context):
         return response['ContentType']
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your
-        bucket is in the same region as this function.'.format(key, bucket))
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
